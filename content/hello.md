5fa5c0da2ee183ad58e43016ecb506f7344d1cda
Chris Lyu <clu@havenagency.com>
2025-02-24 20:44:46 -0800

    init

---
+++ content/hello.md
@@ -0,0 +1,8 @@
+---
+title: Hello from Nuxt Content
+description: This is a simple Markdown-powered page in Nuxt Studio.
+---
+
+# Hello, Nuxt Content!
+
+This page is written in Markdown and rendered dynamically in Nuxt!
---
+++ pages/hello.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data: hello } = await useAsyncData(() => queryCollection('content').path('/hello').first())
+
+useSeoMeta({
+  title: hello.value?.title,
+  description: hello.value?.description
+})
+</script>
+
+<template>
+  <ContentRenderer v-if="hello" :value="hello" />
+  <div v-else>hello not found</div>
+</template>
